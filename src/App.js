ed81e7c17f0c61134e3e42e894a54158dae680a2
Claudia Filipa Cordeiro Alves Fernandes <cfcaf.iefp@alunos.ipb.pt>
2023-09-28 16:49:35 +0100

    add icons to footer funct

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './css/areaDeTrabalho.css';
 import logo from './images/SAAM_SistemaAjudaAdministracaoMedicamentos.png';
+import iconTwitter from './images/twitter_outline_icon.png';
+import iconInstagram from './images/instagram_fill_icon.png';
+import iconFacebook from './images/facebook_outline_icon.png';
+import iconLinkedin from './images/linkedin_outline.png';
+
 
 function header() {
   return (
@@ -36,11 +41,53 @@ function header() {
   )
 }
 
+function Footer(){
+
+
+  return(
+
+    <div className="row">
+            <div className="SAAM_footer">
+                S.A.A.M
+            </div>
+            <footer className="footerFont pt-5">
+                <div className="row text-center">
+                    <div className="col-md-4">
+                        <ul>About us</ul>
+                        <ul>Our services</ul>
+                        <ul>Contact us</ul>
+                        <ul>Legal info</ul>
+                    </div>
+
+                    <div className="col-md-4">
+                        <ul><img src={iconTwitter}/> Twitter</ul>
+                        <ul><img src={iconFacebook}/> Facebook</ul>
+                        <ul><img src={iconInstagram}/> Instagram</ul>
+                        <ul><img src={iconLinkedin}/> Linkedin</ul>
+                    </div>
+
+                    <div className="col-md-4">
+                        <ul>S.A.A.M</ul>
+                        <ul>N 207, Z.I. das Flores,</ul>
+                        <ul>Oliveira de Azemeis</ul>
+                        <ul>4949-494 Whatever</ul>
+                        <ul>Portugal</ul>
+                    </div>
+                </div>
+            </footer>
+        </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       {header()}
+
+
+
+
+      {Footer()}
     </div>
 
     
