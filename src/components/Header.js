52155a6ec6ac739a693d2a1f77f115058769a75c
Willian Fisher Salvi <wfs.iefp@alunos.ipb.pt>
2023-09-29 15:03:31 +0100

    reorganização dos componentes e css

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import logo from '../images/SAAM_SistemaAjudaAdministracaoMedicamentos.png';
+import '../css/Header.css'
+
+function Header() {
+  
+    return (
+  
+      <header className="row navbarRound">
+        <nav className="navbar navbar-expand-md">
+          <div className="container-fluid">
+            <img src={logo} />
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Assistência</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contactos</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Lar Bragança</a>
+                </li>
+              </ul>
+              <div className="pe-2">
+                <button className="btn btn-primary" type="submit">Logout</button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    )
+}
+
+export {Header};
