139fa65843544be610dfb2330f1250bc147ca231
Willian Fisher Salvi <wfs.iefp@alunos.ipb.pt>
2023-10-26 12:25:42 +0100

    adding functionality to notifications

--- src/components/pages/Notificacoes/Notificacoes.js
+++ src/components/pages/Notificacoes/Notificacoes.js
@@ -1,60 +1,65 @@
 import { LoginContext } from "../../../context/LoginContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Navigate } from 'react-router-dom';
 
 
 function Notificacoes() {
     //controle de validação de Login
     const { login } = useContext(LoginContext);
+    const [notificacoes, setNotificacoes] = useState({horarios: false, estoque: false, administracao: false, receita:false});
+
+    useEffect(()=>{
+        try {
+            
+        } catch (e) {
+            console.error(e)
+        }
+    })
+    
+    useEffect(()=>{
+        console.log(notificacoes)
+    },[notificacoes])
 
     if (!login.id || !login.status) {
         return (<Navigate to='/' />)
     }
+    
+    function handlebtn(btnType){
+        const valor = notificacoes[btnType]? false : true
+        setNotificacoes({...notificacoes, [btnType]: valor })
+    }
+
     return (
         <div>
             <h5 class="p-1">Configuração de Alertas</h5>
-            <table class="table table-responsive table-striped">
+            <table className="table table-responsive table-striped">
                 <tbody>
-                    <tr>
-                        <td>Permitir Alertas </td>
-                        <td>
-                            <input type="checkbox" class="btn-check" id="permitirAlertas" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="permitirAlertas">On/Off</label>
-                        </td>
-                    </tr>
                     <tr>
                         <td>Alerta de Horarios </td>
                         <td>
-                            <input type="checkbox" class="btn-check" id="alertaHorario" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="alertaHorario">On/Off</label>
+                            <input type="checkbox" className="btn-check" id="alertaHorario" onClick={()=> handlebtn('horarios')} disabled autocomplete="off" />
+                            <label className="btn btn-outline-success float-end" for="alertaHorario">{notificacoes.horarios? 'ON' : 'OFF'}</label>
                         </td>
                     </tr>
                     <tr>
                         <td>Alerta de Estoque Baixo </td>
                         <td>
-                            <input type="checkbox" class="btn-check" id="estoqueBaixo" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="estoqueBaixo">On/Off</label>
+                            <input type="checkbox" className="btn-check" id="estoqueBaixo" onClick={()=> handlebtn('estoque')} autocomplete="off" />
+                            <label className="btn btn-outline-success float-end" for="estoqueBaixo">{notificacoes.estoque? 'ON' : 'OFF'}</label>
                         </td>
                     </tr>
                     <tr>
                         <td>Falha na Administração </td>
                         <td>
-                            <input type="checkbox" class="btn-check" id="falaAdministracao" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="falaAdministracao">On/Off</label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Mensagens </td>
-                        <td>
-                            <input type="checkbox" class="btn-check" id="mensagens" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="mensagens">On/Off</label>
+                            <input type="checkbox" className="btn-check" id="falaAdministracao" onClick={()=> handlebtn('administracao')} autocomplete="off" />
+                            <label className="btn btn-outline-success float-end" for="falaAdministracao">{notificacoes.administracao? 'ON' : 'OFF'}</label>
                         </td>
                     </tr>
                     <tr>
                         <td>Alerta de Receita Caducada </td>
                         <td>
-                            <input type="checkbox" class="btn-check" id="receitaCaducada" autocomplete="off" />
-                            <label class="btn btn-outline-success float-end" for="receitaCaducada">On/Off</label>
+                            <input type="checkbox" className="btn-check" id="receitaCaducada" onClick={()=> handlebtn('receita')} autocomplete="off" />
+                            <label className="btn btn-outline-success float-end" for="receitaCaducada">{notificacoes.receita? 'ON' : 'OFF'}</label>
                         </td>
                     </tr>
                 </tbody>
