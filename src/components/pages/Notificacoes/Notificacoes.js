f71d29c0ea3cef26a8a7d415e378b532d88dd1f4
Willian Fisher Salvi <wfs.iefp@alunos.ipb.pt>
2023-10-28 22:43:14 +0100

    testing notifications

--- src/components/pages/Notificacoes/Notificacoes.js
+++ src/components/pages/Notificacoes/Notificacoes.js
@@ -6,27 +6,41 @@ import { Navigate } from 'react-router-dom';
 function Notificacoes() {
     //controle de validação de Login
     const { login } = useContext(LoginContext);
-    const [notificacoes, setNotificacoes] = useState({horarios: false, estoque: false, administracao: false, receita:false});
+    const [notificacoes, setNotificacoes] = useState({});
 
-    useEffect(()=>{
-        try {
-            
-        } catch (e) {
-            console.error(e)
+    useEffect(() => {
+        const entidade = async () => {
+            try {
+                const info = await fetch(`http://localhost:5000/api/findEntidade`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ id: login.id }),
+                });
+                const res = await info.json();
+                if (res) {
+                    setNotificacoes(res[0].notificacoes);
+                }
+            } catch (e) {
+                console.error(e)
+            }
         }
-    })
-    
+        entidade()
+    }, [])
+
     useEffect(()=>{
         console.log(notificacoes)
     },[notificacoes])
-
+    
     if (!login.id || !login.status) {
         return (<Navigate to='/' />)
     }
-    
-    function handlebtn(btnType){
-        const valor = notificacoes[btnType]? false : true
-        setNotificacoes({...notificacoes, [btnType]: valor })
+
+    function handlebtn(btnType) {
+        const valor = notificacoes[btnType] ? false : true
+        setNotificacoes({ ...notificacoes, [btnType]: valor })
+        console.log(notificacoes)
     }
 
     return (
@@ -37,29 +51,30 @@ function Notificacoes() {
                     <tr>
                         <td>Alerta de Horarios </td>
                         <td>
-                            <input type="checkbox" className="btn-check" id="alertaHorario" onClick={()=> handlebtn('horarios')} disabled autocomplete="off" />
-                            <label className="btn btn-outline-success float-end" for="alertaHorario">{notificacoes.horarios? 'ON' : 'OFF'}</label>
+                            <button className={`btn btn-outline-success float-end ${notificacoes.horarios ? "active" : ""}`}
+                                onClick={() => handlebtn('horarios')} disabled>{notificacoes.horarios ? 'ON' : 'OFF'}</button>
                         </td>
                     </tr>
                     <tr>
                         <td>Alerta de Estoque Baixo </td>
                         <td>
-                            <input type="checkbox" className="btn-check" id="estoqueBaixo" onClick={()=> handlebtn('estoque')} autocomplete="off" />
-                            <label className="btn btn-outline-success float-end" for="estoqueBaixo">{notificacoes.estoque? 'ON' : 'OFF'}</label>
+
+                            <button className={`btn btn-outline-success float-end ${notificacoes.estoque ? "active" : ""}`}
+                                onClick={() => handlebtn('estoque')}>{notificacoes.estoque ? 'ON' : 'OFF'}</button>
                         </td>
                     </tr>
                     <tr>
                         <td>Falha na Administração </td>
                         <td>
-                            <input type="checkbox" className="btn-check" id="falaAdministracao" onClick={()=> handlebtn('administracao')} autocomplete="off" />
-                            <label className="btn btn-outline-success float-end" for="falaAdministracao">{notificacoes.administracao? 'ON' : 'OFF'}</label>
+                            <button className={`btn btn-outline-success float-end ${notificacoes.administracao ? "active" : ""}`}
+                                onClick={() => handlebtn('administracao')}>{notificacoes.administracao ? 'ON' : 'OFF'}</button>
                         </td>
                     </tr>
                     <tr>
                         <td>Alerta de Receita Caducada </td>
                         <td>
-                            <input type="checkbox" className="btn-check" id="receitaCaducada" onClick={()=> handlebtn('receita')} autocomplete="off" />
-                            <label className="btn btn-outline-success float-end" for="receitaCaducada">{notificacoes.receita? 'ON' : 'OFF'}</label>
+                            <button className={`btn btn-outline-success float-end ${notificacoes.receita ? "active" : ""}`}
+                                onClick={() => handlebtn('receita')}>{notificacoes.receita ? 'ON' : 'OFF'}</button>
                         </td>
                     </tr>
                 </tbody>
