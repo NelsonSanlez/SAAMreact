633b9d5ce6651c45fde3a497e2fe3bf0bcae86cb
Nelson Luis Sanlez da Silva <nlss.iefp@alunos.ipb.pt>
2023-10-30 22:53:41 +0000

    Add changes to pages Historico and  stock. Dynamic, database

--- src/components/pages/Pacientes/Historico/Historico.js
+++ src/components/pages/Pacientes/Historico/Historico.js
@@ -7,124 +7,14 @@ import { Pacientes, pacientes } from '../Pacientes';
 import { Link } from 'react-router-dom';
 import './Historico.css'
 
-// let thisPacienteNbrMensagens
-
-
-// const pacientes = [
-//   {
-//     _id: {
-//       $oid: '6511a70a88e392a4f6ddf847',
-//     },
-//     nome: 'Jane Atkinson',
-//     numUtente: '9758246',
-//     telemovel: '922777888',
-//     email: 'janeatkinson@gmail.com',
-//     medicamento: 'Ben-u-ron',
-//     hora: '15:00',
-//     via: 'Oral',
-//     dose: '1 Comp',
-//     mensagens: [
-//       {
-//         mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//       { mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis' },
-//       {
-//         mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       }
-//     ],
-//   },
-//   {
-//     _id: {
-//       $oid: '6511b31088e392a4f6ddf849',
-//     },
-//     nome: 'Alice Vilar',
-//     numUtente: '6587498',
-//     telemovel: '918452456',
-//     email: 'alicevila@gmail.com',
-//     medicamento: 'Ben-u-ron',
-//     hora: '10:00',
-//     via: 'Oral',
-//     dose: '1 Comp',
-//   },
-//   {
-//     _id: {
-//       $oid: '6511b31088e392a4f6ddf849',
-//     },
-//     nome: 'Alice Vilar',
-//     numUtente: '6587422',
-//     medicamento: 'Ben-u-ron',
-//     hora: '10:00',
-//     via: 'Oral',
-//     dose: '1 Comp',
-//     mensagens: [
-//       {
-//         mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//     ]
-//   },
-//   {
-//     _id: {
-//       $oid: '6511b31088e392a4f6ddf849',
-//     },
-//     nome: 'Alice Vila',
-//     numUtente: '6587421',
-//     telemovel: '918452456',
-//     email: 'alicevila@gmail.com',
-//     medicamento: 'Ben-u-ron',
-//     hora: '10:00',
-//     via: 'Oral',
-//     dose: '1 Comp',
-//     mensagens: [
-//       {
-//         mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex officia voluptate. Culpa nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//       {
-//         mensagem: 'Lorem ipsum dolor eprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//       {
-//         mensagem: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//       {
-//         mensagem: 'Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis',
-//       },
-//     ]
-//   },
-//   {
-//     _id: {
-//       $oid: '6511a70a88e392a4f6ddf847',
-//     },
-//     nome: 'Jane Atkinson',
-//     numUtente: '7346158',
-//     telemovel: '922777888',
-//     email: 'janeatkinson@gmail.com',
-//     medicamento: 'Ben-u-ron',
-//     hora: '15:00',
-//     via: 'Oral',
-//     dose: '1 Comp',
-//     mensagens: [
-//       {
-//         mensagem: 'asdjasdk askjfbkjs kadhndfsk ksdfjnsdfk',
-//       },
-//       { mensagem: 'dsfdnsd dfdfskjngdfogs dfgsndgfs sfgkjnsdfgk' },
-//     ],
-//   },
-// ];
-
-let thisPaciente;
-let thisPacienteNum;
-let thisPacienteEmail;
-let thisPacienteTelemovel;
-let thisPacienteNbrMensagens;
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 const FindThisPaciente = function () {
 
   const [data, setData] = useState([]);
   const { id } = useParams();
   console.log('id after useparams: ' + id);
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     async function FetchPacientes() {
       // async function getData() {
@@ -144,18 +34,15 @@ const FindThisPaciente = function () {
     }
     // const dataFound = 
     FetchPacientes()
-
   }, [])
+//////////////////////////////////////////////////////////////////////////////////////////////
   console.log('id: ' + id);
   console.log('data: ' + data);
   const pacienteFound = data.find((paciente) => paciente.numUtente == id);
   console.log('pacienteFound: ',pacienteFound)
   return pacienteFound
-  
-
- 
 };
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 const Historico = function (props) {
   let pacienteFound= FindThisPaciente();
   //controle de validação de Login
@@ -167,7 +54,7 @@ const Historico = function (props) {
   if (!pacienteFound) {
     return 'Paciente não encontrado!'; // ou uma mensagem de erro adequada
   }
-
+//////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div>
       <div className='numMensagens'><span className='titlesMsg'>Paciente:</span> {pacienteFound.nome} <span className='titlesMsg'>Nº:</span> {pacienteFound.numUtente}</div>
@@ -178,10 +65,9 @@ const Historico = function (props) {
         <div className='textMensagens'><span className='titlesMsg'>Mensagens 3: </span>{pacienteFound.mensagens?.[2]?.mensagem}</div>
         <div className='textMensagens'><span className='titlesMsg'>Mensagens 4: </span>{pacienteFound.mensagens?.[3]?.mensagem}</div>
         <div className='textMensagens'><span className='titlesMsg'>Mensagens 5: </span>{pacienteFound.mensagens?.[4]?.mensagem}</div>
-
       </div>
     </div>
   );
 };
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 export { Historico };
--- src/components/pages/Pacientes/Stock/Stock.js
+++ src/components/pages/Pacientes/Stock/Stock.js
@@ -1,40 +1,78 @@
 import { LoginContext } from "../../../../context/LoginContext";
-import React, { useContext } from "react";
+import React, { useContext,useEffect, useState} from "react";
 import { Navigate } from 'react-router-dom';
+// import { FindThisPaciente } from "../Perfil/Perfil";
+import { useParams } from 'react-router';
+import { Pacientes, pacientes } from '../Pacientes';
+import { Link } from 'react-router-dom';
+import './Stock.css'
 
 
-function Stock() {
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+const FindThisPaciente = function () {
+
+    const [data, setData] = useState([]);
+    const { id } = useParams();
+    console.log('id after useparams: ' + id);
+//////////////////////////////////////////////////////////////////////////////////////////////
+    useEffect(() => {
+    async function FetchPacientes() {
+        // async function getData() {
+        try {
+            const res = await fetch(`http://localhost:5000/listarUtentes`);
+            const pacientes = await res.json();
+            setData(pacientes)
+        } catch (error) {
+            console.error(error);
+        }
+        // }
+        // const dataget = getData()
+        // const dataget2 = await dataget
+        // console.log('data inside  historico: ' + dataget2);
+        // setData(await dataget2)
+        // return data
+    }
+      // const dataFound = 
+    FetchPacientes()
+    }, [])
+//////////////////////////////////////////////////////////////////////////////////////////////
+    console.log('id: ' + id);
+    console.log('data: ' + data);
+    const pacienteFound = data.find((paciente) => paciente.numUtente == id);
+    console.log('pacienteFound: ',pacienteFound)
+    return pacienteFound
+    };
+//////////////////////////////////////////////////////////////////////////////////////////////
+    const Stock = function (props) {
+    let pacienteFound= FindThisPaciente();
     //controle de validação de Login
-   //controle de validação de Login
-   const { login } = useContext(LoginContext);
-    
-   if (!login.id || !login.status) {
-       return (<Navigate to='/'/>)
-   }
-
-
-
+    const { login } = useContext(LoginContext);
+    if (!login.id || !login.status) {
+        return (<Navigate to='/'/>)
+    }
+    if (!pacienteFound) {
+      return 'Paciente não encontrado!'; // ou uma mensagem de erro adequada
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////
     return (
-        <div>Hello!! Aqui o STOCK!!!</div>
-
-
-
-    )
-
-
-}
-
-
-
-
-
-
-
-
-
-
+        <div>
+        <div className='numMensagens'><span className='titlesMsg'>Paciente:</span> {pacienteFound.nome} <span className='titlesMsg'>Nº:</span> {pacienteFound.numUtente}</div>
+        <div >
+            <div className='numMensagens'><span className='titlesMsg'>Nº de Medicamentos:</span> {pacienteFound.medicamentos?.length}</div>
+            <div className='textMensagens'><span className='titlesMsg'>Medicamento 1: {pacienteFound.medicamentos?.[0]?.medicamento} </span>Stock: {pacienteFound.medicamentos?.[0]?.stock}</div>
+            <div className='textMensagens'><span className='titlesMsg'>Medicamento 2: {pacienteFound.medicamentos?.[1]?.medicamento} </span>Stock: {pacienteFound.medicamentos?.[1]?.stock}</div>
+            <div className='textMensagens'><span className='titlesMsg'>Medicamento 3: {pacienteFound.medicamentos?.[2]?.medicamento} </span>Stock: {pacienteFound.medicamentos?.[2]?.stock}</div>
+            <div className='textMensagens'><span className='titlesMsg'>Medicamento 4: {pacienteFound.medicamentos?.[3]?.medicamento} </span>Stock: {pacienteFound.medicamentos?.[3]?.stock}</div>
+            <div className='textMensagens'><span className='titlesMsg'>Medicamento 5: {pacienteFound.medicamentos?.[4]?.medicamento} </span>Stock: {pacienteFound.medicamentos?.[4]?.stock}</div>
+        </div>
+        </div>
+    );
+    };
 
 export default Stock
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
