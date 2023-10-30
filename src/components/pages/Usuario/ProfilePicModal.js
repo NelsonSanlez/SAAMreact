af8ffaeb0c44efeb55d15655330205b34108a425
Nelson Luis Sanlez da Silva <nlss.iefp@alunos.ipb.pt>
2023-10-30 01:41:39 +0000

    Add progress to uploading image of usuario.

--- src/components/pages/Usuario/ProfilePicModal.js
+++ src/components/pages/Usuario/ProfilePicModal.js
@@ -1,37 +1,60 @@
-import { useState } from 'react';
+import {useState,useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Profile from './PerfilUsuario';
+import { storage } from './firebase';
+import { ref, uploadBytes,listAll,getDownloadURL } from 'firebase/storage';
+import { v4 } from 'uuid'
+import './Usuario.css'
+
 
 function ProfilePicModal (props) {
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
-    const [profilePic, setProfilePic] = useState('');
-
+    const [imageUpload, setImageUpload] = useState(null)
+    const [imageList,setImageList] = useState([])
+    const [profPicUrl,setProfPicUrl] = useState()
 
     const userEdit = {
         id: props.usuario._id,
-        profilePic: profilePic,
-
+        profilePic: profPicUrl,
     }
 
+    //aqui quero enviar o url da imagem do user para a database
     const handleSubmit = async () => {
         try {
-
+            setProfPicUrl(imageList)
             const edit = await fetch("http://localhost:5000/api/editUsuario", { method: "PUT", body: JSON.stringify(userEdit), headers: { "content-type": "application/json" } });
             const res = await edit.json();
             handleClose();
             return res
-
         }catch(e){
             console.error(e)
         }
   }
 
+
+const imageListRef = ref(storage, 'saam_prof_pics/${props.usuario.ultimoNome}_${props.usuario.primeiroNome}-${props.usuario.idNurse}/')
+const uploadImage = ()=>{
+  if (imageUpload==null) return
+  const imageRef = ref(storage, `saam_prof_pics/${props.usuario.ultimoNome}_${props.usuario.primeiroNome}-${props.usuario.idNurse}/${imageUpload.name+v4()}`)
+  uploadBytes (imageRef,imageUpload).then ((snapshot)=>{
+    alert('Image Uploaded!')
+    getDownloadURL(snapshot.ref).then((url)=>{
+    //   setImageList((prev)=>[...prev,url])
+    setImageList([url])
+    })
+  })
+}
+
+useEffect(()=>{
+  listAll(imageListRef).then((response)=>{
+    console.log(response)
+    response.items.forEach((item)=>{getDownloadURL(item).then((url)=>{setImageList((prev)=>[...prev, url])})}) 
+  })
+},[])
 
 
     return (
@@ -44,15 +67,10 @@ function ProfilePicModal (props) {
                 <Modal.Header closeButton>
                     <Modal.Title>Escolha a sua imagem de perfil:</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    
-                    <Profile  
-                     type="file"
-                                placeholder={`${props.usuario.profilePic}`}
-                                onChange={(e) => setProfilePic(e.target.value)}
-                                autoFocus/>
+                <Modal.Body>                    
+                    {/* <Profile type="file" placeholder={`${props.usuario.profilePic}`} onChange={(e) => setProfilePic(e.target.value)} autoFocus/> */}
 
-                    <Form>
+                    {/* <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Escolha a sua imagem de perfil:</Form.Label>
                             <Form.Control
@@ -62,8 +80,16 @@ function ProfilePicModal (props) {
                                 autoFocus
                             />
                         </Form.Group>
+                    </Form> */}
 
-                    </Form>
+    <div className="App">
+      <input type='file' onChange={(event)=>{setImageUpload(event.target.files[0])}}/>
+      <button onClick={uploadImage} >Upload image</button>
+
+      {imageList.map((url)=>{
+        return <img className='imgUploadProfPic' src={url}/>
+      })}
+    </div>
 
                 </Modal.Body>
                 <Modal.Footer>
--- src/components/pages/Usuario/Usuario.js
+++ src/components/pages/Usuario/Usuario.js
@@ -1,7 +1,7 @@
 import { useState, useContext, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Button } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
 import { LoginContext } from '../../../context/LoginContext';
 import EditModal from "./EditModal";
 import ProfilePicModal from "./ProfilePicModal";
@@ -13,8 +13,22 @@ const UsuarioPage = () => {
 
     const { login } = useContext(LoginContext);
     const [usuario, setUsuario] = useState({});
+    const [customProfPic, setCustomProfPic] = useState(true)
 
-
+const UsedPorfPic = ()=>{
+    if (customProfPic === true) {
+        return(
+            <div className='imgIniciaisUsuarioDiv'>
+                {/* <img className='imgCustomUsuario' src={`https://firebasestorage.googleapis.com/v0/b/uploadingfile-746c0.appspot.com/o/saam_prof_pics%2FSanlez_Nelson-65329f8a64c1710afd2a1362%2F61DNV%2BZSd8L._AC_UF1000%2C1000_QL80_.jpgd7d319e9-5fc3-4154-b202-418069d7dbb4?alt=media&token=07d78dc9-67f4-48a8-9f73-c848547404e6&_gl=1*1u8gq1p*_ga*MzQ3NzYxMjIzLjE2OTU5NzgwNTA.*_ga_CW55HF8NVT*MTY5ODYyMzI5Ny4xMC4xLjE2OTg2MjY1NzAuNTUuMC4w`} alt="profile_pic"/> */}
+                <img className='imgCustomUsuario' src={usuario.profilePic} alt="profile_pic"/>
+            </div>
+                )}
+    return(
+        <div className='imgIniciaisUsuarioDiv'>
+            <img className='imgIniciaisUsuario' src={`https://ui-avatars.com/api/?size=128&rounded=true&background=random&color=random&bold=true&length=3&name=${usuario.primeiroNome}+${usuario.ultimoNome}`} alt="profile_pic"/>
+        </div>
+    )
+}
 
 
     useEffect(() => {
@@ -45,9 +59,8 @@ const UsuarioPage = () => {
             <div className="info">
                 <Card style={{ width: '100%' }}>
                     <ListGroup variant="flush">
-                        <ListGroup.Item><div className='imgIniciaisUsuarioDiv'>
-                            <img className='imgIniciaisUsuario' src={`https://ui-avatars.com/api/?size=128&rounded=true&background=random&color=random&bold=true&length=3&name=${usuario.primeiroNome}+${usuario.ultimoNome}`} />
-                        </div><h3>Imagem de Perfil</h3></ListGroup.Item>
+                        <ListGroup.Item><UsedPorfPic/></ListGroup.Item>
+                        <ListGroup.Item><h3>Imagem de Perfil</h3></ListGroup.Item>
                         <ListGroup.Item><h3>Nome:</h3><p>{`${usuario.primeiroNome}`}</p></ListGroup.Item>
                         <ListGroup.Item><h3>Sobrenome:</h3><p>{`${usuario.ultimoNome}`}</p></ListGroup.Item>
                         <ListGroup.Item><h3>Cédula de Enfermeiro:</h3><p>{`${usuario.cedulaPro}`}</p></ListGroup.Item>
